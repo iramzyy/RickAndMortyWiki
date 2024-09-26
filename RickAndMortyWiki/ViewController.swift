9c88d547ed46778fcbc117d1d947141c0954080e
Ramzy <ramzy.2871@gmail.com>
2024-09-26 17:58:55 +0300

    Init commit 🚀

---
+++ RickAndMortyWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyWiki
+//
+//  Created by Ahmed Ramzy on 26/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
